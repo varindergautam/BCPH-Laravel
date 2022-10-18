a0767ae8fe1ea6f108cccb3d252b8d28217f9157
varindergautam <varindergautam48@gmail.com>
2022-10-18 22:54:56 +0530

    18 oct

--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Srmklive\PayPal\Providers\PayPalServiceProvider::class,
+        Anand\LaravelPaytmWallet\PaytmWalletServiceProvider::class,
     ],
 
     /*
@@ -229,6 +231,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'PayPal' => Srmklive\PayPal\Facades\PayPal::class,
+        'PaytmWallet' => Anand\LaravelPaytmWallet\Facades\PaytmWallet::class,
 
     ],
 
--- resources/views/layouts/front_layout.blade.php
+++ resources/views/layouts/front_layout.blade.php
@@ -9,6 +9,8 @@
     <link href="{{ asset('common/plugins/css/sweetalert2.min.css') }}" rel="stylesheet">
     <link href="{{ asset('front/css/style.css?v='.rand(1,99)) }}" rel="stylesheet">
     <link href="{{ asset('front/css/responsive.css?v='.rand(1,99)) }}" rel="stylesheet">
+    <link href="{{ asset('front/theme_css/style.css?v='.rand(1,99)) }}" rel="stylesheet">
+
 
     <script src="{{ asset('common/plugins/js/jquery.min.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.blockUI/2.70/jquery.blockUI.min.js"></script>
@@ -42,6 +44,68 @@
     display:none">
     </div>
 
+    <nav class="" style="    box-shadow: 0px 2px 40px 0px hsl(0deg 0% 6% / 5%);">
+        <div class="topbar tb-border-design visible-on-mobile">
+           <div class="container" >
+              <div class="topbar-left-content ">
+                 <div class="topbar-contact"><span class="topbar-phone"><a href="tel:+91(0)172-2688519"><i class="sway-phone-topbar fas" aria-hidden="true"></i><span>+91(0)172-2688519</span></a></span><span class="topbar-phone"><a href="tel:+91-8195017269"><i class="sway-phone-topbar fas" aria-hidden="true"></i><span>+91-8195017269</span></a></span><span class="topbar-email"><a href="mailto:lawbhawan37@gmail.com"><i class="sway-mail-topbar far" aria-hidden="true"></i><span>lawbhawan37@gmail.com</span></a></span><span class="topbar-opening-hours"><i class="sway-schedule-topbar far" aria-hidden="true"></i><span>Monday to Saturday 10:00 AM – 5:00 PM, Sunday - Closed</span></span></div>
+              </div>
+              <div class="topbar-right-content ">
+                 <div class="topbar-socials">
+                    <ul class="redux-social-media-list clearfix"></ul>
+                 </div>
+              </div>
+           </div>
+        </div>
+        <div class="navbar navbar-expand-lg menubar main-nav-right">
+           <div class="container" style="max-width: 1240px;">
+              <div id="logo">
+                 <a class="logo" href="http://112.196.117.52/bcph_website" title="BCPH">BCPH</a>
+              </div>
+              <div class="navbar-header page-scroll">
+                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#main-menu">
+                     <span class="icon-bar"></span>
+                     <span class="icon-bar"></span>
+                     <span class="icon-bar"></span>
+                     </button>
+     
+                     
+                     <div class="mobile-cart">
+                                       </div>
+                             </div>
+              <div id="main-menu" class="collaps1e navbar-collapse background-dropdown-effect justify-content-end">
+                 <ul id="menu-logged-in-main-menu" class="nav navbar-nav">
+                    <li id="menu-item-9782" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-6 current_page_item menu-item-9782 active"><a href="http://112.196.117.52/bcph_website/">Home</a></li>
+                    <li id="menu-item-9784" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9784"><a href="http://112.196.117.52/bcph_website/about-us/">About Us</a></li>
+                    <li id="menu-item-9791" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9791"><a href="http://112.196.117.52/bcph_website/members/">Members</a></li>
+                    <li id="menu-item-9790" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-9790 dropdown">
+                       <a href="http://112.196.117.52/bcph_website/events/">Events</a>
+                       <ul role="menu" class=" dropdown-menu">
+                          <li id="menu-item-9796" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9796"><a href="http://112.196.117.52/bcph_website/event-dashboard/">Event Dashboard</a></li>
+                          <li id="menu-item-9788" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9788"><a href="http://112.196.117.52/bcph_website/event-organizers/">Event Organizers</a></li>
+                          <li id="menu-item-9789" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9789"><a href="http://112.196.117.52/bcph_website/event-venues/">Event Venues</a></li>
+                          <li id="menu-item-9792" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9792"><a href="http://112.196.117.52/bcph_website/organizer-dashboard/">Organizer Dashboard</a></li>
+                          <li id="menu-item-9793" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9793"><a href="http://112.196.117.52/bcph_website/post-an-event/">Post an Event</a></li>
+                       </ul>
+                       <span class="mobile-dropdown"></span>
+                    </li>
+                    <li id="menu-item-9786" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9786"><a href="http://112.196.117.52/bcph_website/downloads/">Downloads</a></li>
+                    <li id="menu-item-9783" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9783"><a href="http://112.196.117.52/bcph_website/blog/">Blog</a></li>
+                    <li id="menu-item-9829" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9829"><a href="http://112.196.117.52/bcph_website/useful-links/">Useful Links</a></li>
+                    <li id="menu-item-9785" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9785"><a href="http://112.196.117.52/bcph_website/contact-us/">Contact Us</a></li>
+                 </ul>
+              </div>
+              <div class="main-nav-extra-content">
+                 <div class="search-cart-wrapper">
+                 </div>
+                 <div class="header-bttn-wrapper">
+                    <a class="modal-menu-item tt_button tt_primary_button btn_primary_color default_header_btn default_header_btn" target="_blank" href="http://112.196.117.52/bcph/public" style="color: #fff; background: #C8102E; font-weight: 500;">Apply Now</a>
+                 </div>
+              </div>
+           </div>
+        </div>
+     </nav>
+
     @auth
         <header class="d-print-none">
             <div class="container py-2">
@@ -63,10 +127,93 @@
     @endauth
 
     @yield('content')
+
+    <footer id="footer" class="fixed underline-effect">
+        <div class="upper-footer" style="">
+           <div class="container">
+              <!-- <div class="footer-bar ">
+                 <div class="footer-nav-menu">
+                    <ul id="menu-footer-menu" class="navbar-footer">
+                       <li id="menu-item-8923" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-8923"><a href="#">About</a></li>
+                       <li id="menu-item-8927" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-8927"><a href="#">Contact us</a></li>
+                    </ul>
+                 </div>
+                 <div class="footer-socials-bar">
+                    <ul class="redux-social-media-list clearfix"></ul>
+                 </div>
+              </div> -->
+              <div class="footer-widget-area row">
+                 <div class="first-widget-area footer-widget-column col-xs-12 col-sm-12 col-md-4 col-lg-4 text-left">
+                    <div id="custom_html-2" class="widget_text footer_widget widget widget_custom_html">
+                       <h5 class="widget-title"><span>ABOUT US</span></h5>
+                       <div class="textwidget custom-html-widget">The Bar Council of Punjab &amp; Haryana, Chandigarh is an Autonomous body est-
+                          ablished and constituted in the year 1961 under the provisions of Advocates Ac-
+                          t 1961 which provide for establishment of State Bar Councils in Section 3 there-
+                          of with a maximum of 25 elected members. 
+                       </div>
+                    </div>
+                 </div>
+                 <div class="second-widget-area footer-widget-column col-xs-12 col-sm-12 col-md-2 col-lg-2 text-left">
+                    <div id="nav_menu-1" class="footer_widget widget widget_nav_menu">
+                       <h5 class="widget-title"><span>IMPORTANT LINK</span></h5>
+                       <div class="menu-main-menu-container">
+                          <ul id="menu-main-menu" class="menu">
+                             <li id="menu-item-9490" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-9490"><a href="http://112.196.117.52/bcph_website/" aria-current="page">Home</a></li>
+                             <li id="menu-item-9743" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9743"><a href="http://112.196.117.52/bcph_website/about-us/">About Us</a></li>
+                             <li id="menu-item-9742" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9742"><a href="http://112.196.117.52/bcph_website/members/">Members</a></li>
+                             <li id="menu-item-9776" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9776"><a href="http://112.196.117.52/bcph_website/events/">Events</a></li>
+                             <li id="menu-item-9761" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9761"><a href="http://112.196.117.52/bcph_website/downloads/">Downloads</a></li>
+                             <li id="menu-item-9740" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9740"><a href="http://112.196.117.52/bcph_website/blog/">Blog</a></li>
+                             <li id="menu-item-9741" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-9741"><a href="http://112.196.117.52/bcph_website/contact-us/">Contact Us</a></li>
+                          </ul>
+                       </div>
+                    </div>
+                 </div>
+                 <div class="third-widget-area footer-widget-column col-xs-12 col-sm-12 col-md-2 col-lg-2 text-left">
+                    <div id="custom_html-1" class="widget_text footer_widget widget widget_custom_html">
+                       <h5 class="widget-title"><span>Contact</span></h5>
+                       <div class="textwidget custom-html-widget">Dakshin Marg,<br>
+                          Sector 37A, Sector 37,<br> 
+                          Chandigarh, 160036
+                       </div>
+                    </div>
+                 </div>
+                 <div class="fourth-widget-area footer-widget-column col-xs-12 col-sm-12 col-md-2 col-lg-2 text-left">
+                    <div id="text-3" class="footer_widget widget widget_text">
+                       <h5 class="widget-title"><span>Reach Us</span></h5>
+                       <div class="textwidget">
+                          <p><iframe loading="lazy" style="border: 0;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13716.587870265705!2d76.755!3d30.742373000000004!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7889dfe360751e03!2sBar%20Council%20of%20Punjab%20and%20Haryana%20Chandigarh!5e0!3m2!1sen!2sin!4v1664616200899!5m2!1sen!2sin" allowfullscreen=""></iframe></p>
+                       </div>
+                    </div>
+                 </div>
+              </div>
+           </div>
+        </div>
+        <div class="lower-footer copyright-center">
+           <div class="container">
+              <span>
+              Sway by KeyDesign. All rights reserved.           </span>
+           </div>
+        </div>
+     </footer>
 					
     <script>
         var baseUrl = "{{url('/') }}";
     </script>
+    <script type="text/javascript">
+        $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+    
+         //>=, not <=
+        if (scroll >= 0) {
+            //clearHeader, not clearheader - caps H
+            $(".main-nav-right").addClass("darkHeader");
+        }
+        else {
+          $(".main-nav-right").removeClass("darkHeader");
+        }
+    }); //missing );
+      </script>
     @yield('script')
 </body>
 </html>
